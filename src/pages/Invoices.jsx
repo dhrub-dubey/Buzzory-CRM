cbbf9dab7abae43b4ac7d53dca2d60e96ed1d9d9
Watson-Dhrub <dhrubdubey5997@gmail.com>
2026-07-26 20:13:51 +0530

    update

--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
 import { FileText, Plus, Download, Archive } from 'lucide-react';
+import { LoaderCircle } from "lucide-react";
 import { Button } from '@/components/ui/button';
 import { CircleCheck } from "lucide-react";
 import jsPDF from "jspdf";
@@ -127,7 +128,12 @@ export default function Invoices() {
 
   const downloadInvoiceWithToast = (invoice) => {
     const toastId = toast.loading(
-      `${invoice.invoice_number}.pdf is being downloaded...`
+      `${invoice.invoice_number}.pdf is being downloaded...`,
+      {
+        icon: (
+          <LoaderCircle className="w-5 h-5 text-orange-500 animate-spin" />
+        ),
+      }
     );
   
     setDownloadInvoice({
