086c37afd3ecc01a6561ad0fb69cc51e3cf32dbe
Watson-Dhrub <dhrubdubey5997@gmail.com>
2026-07-26 15:34:06 +0530

    bug fixed again

--- src/pages/Influencers.jsx
+++ src/pages/Influencers.jsx
@@ -489,7 +489,7 @@ export default function Influencers() {
       {/* Results Header */}
       <div className="flex items-center justify-between mb-3">
         <div className="flex items-center gap-2">
-          <h2 className="text-base font-semibold"><h2>
+          <h2 className="text-base font-semibold">
             {cityFilter
               ? `${cityFilter} Influencers`
               : catFilter !== "all"
